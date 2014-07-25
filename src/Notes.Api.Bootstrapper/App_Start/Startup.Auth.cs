e5be700c1853c2dd54148edd8028530aca382a99
smith.h.neil <smith.h.neil@gmail.com>
2014-07-25 08:56:38 -0500

    chore(bootstrap): create startup classes

---
+++ src/Notes.Api.Bootstrapper/App_Start/Startup.Auth.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+
+namespace Notes.Api.Bootstrapper
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app) {
+            
+            
+
+        }
+    }
+}
---
+++ src/Notes.Api.Bootstrapper/App_Start/Startup.cs
@@ -0,0 +1,11 @@
+﻿using Owin;
+
+namespace Notes.Api.Bootstrapper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
