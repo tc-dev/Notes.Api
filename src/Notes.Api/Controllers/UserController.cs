f1ba983a1195bbe87af755824850061846726c25
smith.h.neil <smith.h.neil@gmail.com>
2014-07-25 01:14:49 -0500

    chore(api): set up single endpoint used to set up bootstrapper

---
+++ src/Notes.Api/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Notes.Api.Core.Data;
+using Notes.Api.Core.Data.Repositories;
+using tc_dev.Core.Common.Utilities;
+
+namespace Notes.Api.Controllers
+{
+    [RoutePrefix("api/user")]
+    public class UserController
+    {
+        private readonly INotesUnitOfWork _unitOfWork;
+        private readonly IDomainUserRepository _domainUserRepo;
+
+        public UserController(INotesUnitOfWork unitOfWork) {
+            unitOfWork.ThrowIfNull("unitOfWork");
+
+            _unitOfWork = unitOfWork;
+            _domainUserRepo = unitOfWork.DomainUserRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            return new[] {"user1", "user2"};
+        }
+    }
+}
