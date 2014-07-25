2b9829a5d156814c61c8d581048152d29b156d5a
smith.h.neil <smith.h.neil@gmail.com>
2014-07-25 14:32:52 -0500

    feat(api): add NoteController

---
+++ src/Notes.Api/Controllers/NoteController.cs
@@ -0,0 +1,120 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Notes.Api.Core.Data;
+using Notes.Api.Core.Data.Repositories;
+using Notes.Api.Core.Domain.Models;
+using Notes.Api.Models;
+using tc_dev.Core.Common.Utilities;
+using tc_dev.Core.Identity;
+
+namespace Notes.Api.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/note")]
+    public class NoteController : ApiController
+    {
+        private readonly INotesUnitOfWork _unitOfWork;
+        private readonly INoteRepository _noteRepository;
+        private readonly IAppUserManager _userManager;
+
+        public NoteController(INotesUnitOfWork unitOfWork, IAppUserManager userManager) {
+            unitOfWork.ThrowIfNull("unitOfWork");
+            userManager.ThrowIfNull("userManager");
+
+            _unitOfWork = unitOfWork;
+            _noteRepository = unitOfWork.NoteRepository;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IHttpActionResult> Get(int id) {
+            var noteEntity = await _noteRepository.Query()
+                .ById(id)
+                .FirstOrDefaultAsync();
+
+            if (noteEntity == null)
+                return NotFound();
+
+            var model = new NoteModel {
+                Id = noteEntity.Id,
+                PageId = noteEntity.PageId,
+                DateCreated = noteEntity.DateCreated,
+                DateLastModified = noteEntity.DateLastModified,
+                Text = noteEntity.Text
+            };
+
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Post(NoteBindingModel model) {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try {
+                var note = new Note {
+                    PageId = model.PageId,
+                    Text = model.Text
+                };
+
+                _noteRepository.Insert(note);
+                await _unitOfWork.SaveChangesAsync();
+
+                var uri = Url.Link("DefaultApi", new {id = note.Id});
+                return Created(uri, note);
+            }
+            catch {
+                return InternalServerError(); // TODO
+            }
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IHttpActionResult> Put(int id, NoteBindingModel model) {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try {
+                var noteEntity = await _noteRepository.Query()
+                    .ById(id)
+                    .FirstOrDefaultAsync();
+
+                if (noteEntity == null)
+                    return NotFound();
+
+                noteEntity.PageId = model.PageId;
+                noteEntity.Text = model.Text;
+
+                _noteRepository.Update(noteEntity);
+                await _unitOfWork.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch {
+                return InternalServerError(); // TODO
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IHttpActionResult> Delete(int id) {
+            try {
+                var noteEntity = await _noteRepository.Query()
+                    .ById(id)
+                    .FirstOrDefaultAsync();
+
+                if (noteEntity == null)
+                    return NotFound();
+
+                _noteRepository.Remove(noteEntity);
+                await _unitOfWork.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch {
+                return InternalServerError(); // TODO
+            }
+        }
+    }
+}
---
+++ src/Notes.Api/Models/NoteModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Notes.Api.Models
+{
+    public class NoteModel
+    {
+        public int Id { get; set; }
+
+        public int PageId { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime? DateLastModified { get; set; }
+
+        public string Text { get; set; }
+    }
+
+    public class NoteBindingModel
+    {
+        [Required]
+        public int PageId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+    }
+}
