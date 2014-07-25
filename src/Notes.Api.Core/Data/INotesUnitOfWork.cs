4da189a09d3a5088138851bb9eef7c7ba29f0ca0
smith.h.neil <smith.h.neil@gmail.com>
2014-07-25 00:10:10 -0500

    feat(core): add INotesUnitOfWork interface

---
+++ src/Notes.Api.Core/Data/INotesUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Notes.Api.Core.Data.Repositories;
+using tc_dev.Core.Data;
+
+namespace Notes.Api.Core.Data
+{
+    public interface INotesUnitOfWork : IUnitOfWork
+    {
+        IDomainUserRepository DomainUserRepository { get; }
+
+        INoteBookRepository NoteBookRepository { get; }
+
+        IPageRepository PageRepository { get; }
+
+        INoteRepository NoteReposigory { get; }
+    }
+}
