a1aff35396ae0914a0d038082719033111690ea6
smith.h.neil <smith.h.neil@gmail.com>
2014-07-25 09:53:56 -0500

    feat(api): add AccountController & Register endpoint

---
+++ src/Notes.Api/Controllers/AccountController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Notes.Api.Models;
+using tc_dev.Core.Identity;
+using tc_dev.Core.Identity.Models;
+
+namespace Notes.Api.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        private readonly IAppUserManager _userManager;
+        private readonly IAppAuthenticationManager _authenticationManager;
+
+        public AccountController(IAppUserManager userManager, IAppAuthenticationManager authManager) {
+            _userManager = userManager;
+            _authenticationManager = authManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(RegisterBindingModel model) {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = new AppUser {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+                return GetErrorResult(result);
+
+            await _userManager.AddToRoleAsync(user.Id, "UserRole");
+            return Ok();
+        }
+
+
+
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                _userManager.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Helpers
+
+        private IHttpActionResult GetErrorResult(AppIdentityResult result) {
+            if (result == null) {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded) {
+                if (result.Errors != null) {
+                    foreach (string error in result.Errors) {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid) {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+
+        private static class RandomOAuthStateGenerator
+        {
+            private static RandomNumberGenerator _random = new RNGCryptoServiceProvider();
+
+            public static string Generate(int strengthInBits) {
+                const int bitsPerByte = 8;
+
+                if (strengthInBits % bitsPerByte != 0) {
+                    throw new ArgumentException("strengthInBits must be evenly divisible by 8.", "strengthInBits");
+                }
+
+                int strengthInBytes = strengthInBits / bitsPerByte;
+
+                byte[] data = new byte[strengthInBytes];
+                _random.GetBytes(data);
+                return HttpServerUtility.UrlTokenEncode(data);
+            }
+        }
+
+        #endregion
+    }
+}
